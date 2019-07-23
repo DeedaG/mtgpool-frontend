066fffd1499d104f07490dcbf5cc720b1c394c86
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-23 14:41:01 -0500

    added Login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login.js"
 
 class App extends React.Component {
 
@@ -13,7 +14,7 @@ class App extends React.Component {
 
   render() {
   return (
-    "React"
+    <Login />
   );
  }
 }
