40ca954a5ad6d36e750a4364d9db0345eda62a81
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-23 09:18:31 -0500

    created current user reducer for login

---
+++ src/actions/currentUser.js
@@ -0,0 +1,14 @@
+//synchronous action creators
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
+
+//asynchronous action creators
+export const login = info => {
+  return {
+    return fetch("http://localhost:3000/")
+  }
+}
