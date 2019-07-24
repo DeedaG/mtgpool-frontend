947410d0d24d328a2bd0aacfb73c49f6363e8884
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-24 17:20:20 -0500

    created ternary to render login option or logged off

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login.js"
+import Logout from "./components/Logout.js"
 import { connect } from 'react-redux'
 import { getCurrentUser} from './actions/currentUser.js'
 
@@ -13,10 +14,19 @@ class App extends React.Component {
 
 
   render() {
-  return (
-    <Login />
-  );
+    return (
+      // navbar?
+      this.props.currentUser ? <Logout /> : <Login />
+    );
  }
 }
 
-export default connect(null, { getCurrentUser })(App);
+// only calling one piece from state, incoming arguemnt is an argument coming from redux
+// property called currentUser
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
