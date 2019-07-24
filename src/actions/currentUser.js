0f47f12b13e75ba1e143a3ae5aacc646dded8b53
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-23 22:00:45 -0500

    added two actions for logging in and getting current user

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -9,13 +9,46 @@ export const setCurrentUser = user => {
 //asynchronous action creators
 export const login = info => {
   return dispatch => {
-    return fetch(
-      "http://localhost:3000/api/v1/login" ,{
+    console.log(info)
+    return fetch("http://localhost:3000/api/v1/login",
+    {
+      credentials: "include",
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify()
+      body: JSON.stringify(info)
     })
+      .then(r => r.json())
+      .then(user => {
+        if (user.error) {
+          alert(user.error)
+        } else {
+          dispatch(setCurrentUser(user))
+        }
+      })
+      .catch(console.log)
+    }
   }
-}
+
+  export const getCurrentUser = () => {
+    return dispatch => {
+      return fetch("http://localhost:3000/api/v1/get_current_user",
+      {
+        credentials: "include",
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(r => r.json())
+        .then(user => {
+          if (user.error) {
+            alert(user.error)
+          } else {
+            dispatch(setCurrentUser(user))
+          }
+        })
+        .catch(console.log)
+      }
+    }
