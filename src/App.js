2bd41a122aad75dc7a13bed12ba81ea492e374fe
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 11:56:09 -0500

    moved login logout logic to navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import logo from './logo.svg';
+import NavBar from './components/NavBar.js';
+import MainContainer from './components/MainContainer.js';
 import './App.css';
-import Login from "./components/Login.js"
-import Logout from "./components/Logout.js"
 import { connect } from 'react-redux'
 import { getCurrentUser} from './actions/currentUser.js'
+
 
 class App extends React.Component {
 
@@ -15,18 +15,19 @@ class App extends React.Component {
 
   render() {
     return (
-      // navbar?
-      this.props.currentUser ? <Logout /> : <Login />
+      <div className="App">
+        <NavBar/>
+        <MainContainer/>
+      </div>
+
+
+    // <Footer/>
     );
  }
 }
 
 // only calling one piece from state, incoming arguemnt is an argument coming from redux
 // property called currentUser
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+
+export default connect(null, { getCurrentUser })(App);
