c559597a30acef7104721d4cdf25aaeafcbd40a0
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 20:55:05 -0500

    did not save changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,13 @@ import Logout from "./Logout.js"
 import { connect } from 'react-redux'
 
 
+
   const NavBar = ( { currentUser }) => {
     return (
       <div className="NavBar">
         { currentUser ? <strong>Welcome, {currentUser.attributes.username}</strong> : "" }
         { currentUser ? <Logout /> : <Login /> }
+
       </div>
     )
 }
