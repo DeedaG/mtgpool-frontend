9308d5d847f52efb969a4f2f2377cf3e2ab154a7
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 20:48:07 -0500

    removed navbar added routes for login, my pools, new pool

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import NavBar from './components/NavBar.js';
-import MainContainer from './components/MainContainer.js';
+// import NavBar from './components/NavBar.js';
+// import MainContainer from './components/MainContainer.js';
 import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser} from './actions/currentUser.js'
-
+import Login from './components/Login.js'
+import MyPools from './components/MyPools.js'
+// import PoolCard from './components/PoolCard.js'
+import Home from './components/Home.js'
+import NewPoolForm from './components/NewPoolForm.js'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -14,11 +19,32 @@ class App extends React.Component {
 
 
   render() {
+    const { loggedIn } = this.props
+
     return (
       <div className="App">
-        <NavBar/>
-        <MainContainer/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={(props)=>loggedIn ? <MyPools/> : <Home/>}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/my-pools' component={MyPools}/>
+        <Route exact path='/pools/new' component={NewPoolForm}/>
+      </Switch>
+    </div>
+          // <Route exact path='/Pools/:id' render={props => {
+            //     // I need to get ???
+            //   const pool = pools.find(pool => pool.id === props.match.params.id)
+            //     console.log(pool)
+            //     return <PoolCard pool={pool} {...props}/>
+            //     }
+            //   }/>
+            // <Route exact path='/pools/:id/edit' render={props => {
+            //     // I need to get ???
+            //     const pool = pools.find(pool => pool.id === props.match.params.id)
+            //     // dispatch updateForm -> trip
+            //     return <EditPoolFormWrapper pool={pool} {...props}/>
+            //   }
+            // }/>
+
 
 
     // <Footer/>
@@ -26,8 +52,11 @@ class App extends React.Component {
  }
 }
 
-// only calling one piece from state, incoming arguemnt is an argument coming from redux
-// property called currentUser
+  const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+    })
+  }
 
 
-export default connect(null, { getCurrentUser })(App);
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
