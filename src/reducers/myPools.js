48f70e09a14a82421f3bfe34be545b3abe135bf9
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-28 17:06:29 -0500

    created case statement for clearMyPools

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case "SET_MY_POOLS":
       return action.pools
+    case "CLEAR_MY_POOLS":
+      return []
     default:
         return state
     }
