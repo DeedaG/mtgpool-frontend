85972ebee4b39c8ac0d6f9f92616840bc28f5f33
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-28 17:01:34 -0500

    tweaked Home, will only log in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Login from './Login.js'
+// import Login from './Login.js'
 import { Link } from 'react-router-dom'
 
 const Home = () => (
   <div>
-    <h4>Welcome, please <Link to="/Login"> Log In </Link> </h4>
+    <h4>Welcome, please <Link to="/login"> Log In </Link> </h4>
   </div>
 )
 export default Home;
