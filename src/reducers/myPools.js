96bd09493e0acf0889713af1f7d42c3488828bf3
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-29 15:00:50 -0500

    corrected ADDPOOL concat(action.pools)

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -1,9 +1,13 @@
+const initialState = []
+
 export default (state = [], action) => {
   switch(action.type) {
     case "SET_MY_POOLS":
       return action.pools
+    case "ADD_POOL":
+      return state.pools.concat(action.pools)
     case "CLEAR_MY_POOLS":
-      return []
+      return initialState
     default:
         return state
     }
