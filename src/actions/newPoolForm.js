9c0360734eb1b12554740ca54dc4570302be8882
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-29 14:59:28 -0500

    added dropdown to choose investor for new pool form

--- src/actions/newPoolForm.js
+++ src/actions/newPoolForm.js
@@ -1,6 +1,8 @@
 export const  updateNewPoolForm = (name, value) => {
+  const formData = {name,value}
+
   return {
     type: "UPDATE_NEW_POOL_FORM",
-    formData: { name, value }
+    formData
   }
 }
