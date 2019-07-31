631bedbabfb2103f21ff03ed768674a70002d6da
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 21:57:24 -0500

    added link to view each pool

--- src/components/MyPools.js
+++ src/components/MyPools.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import PoolCard from './PoolCard.js'
+// import PoolCard from './PoolCard.js'
+import { Link } from 'react-router-dom'
 
 
 const MyPools = props => {
   const poolCards = props.pools.length > 0 ? props.pools.map(p =>
-    <PoolCard pool={p} key={p.id} />) : null
+   <Link to ={`/pools/${p.id}`} key={p.id}>{p.attributes.name}</Link>) : null
   return (
 
     poolCards
-    
+
   )
 }
 
