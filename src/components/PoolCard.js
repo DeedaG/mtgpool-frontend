56fc28449963b48e5a32796ca783dcc87e5b2e71
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 21:55:40 -0500

    added link to add loans to pool

--- src/components/PoolCard.js
+++ src/components/PoolCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PoolCard = ({pool}) => {
   return (
-    <h3>{pool.attributes.name}</h3>
+    pool ?
+    <div>
+      <h3>{pool.attributes.name}</h3>
+      <p>{pool.attributes.pool_amount}</p>
+      <Link to={`/pools/${pool.id}/edit`}>Edit this pool</Link><br></br>
+      <Link to={'/loans'}>Add Loans</Link>
+    </div> :
+    null
   )
 }
 
