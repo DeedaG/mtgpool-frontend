99afeb03630c12f37ed97b2537df1e1b14b45f71
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 21:58:22 -0500

    clear my pools

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -5,9 +5,10 @@ export default (state = [], action) => {
     case "SET_MY_POOLS":
       return action.pools
     case "ADD_POOL":
-      return state.pools.concat(action.pools)
+      return state.concat(action.pool)
     case "CLEAR_MY_POOLS":
       return initialState
+
     default:
         return state
     }
