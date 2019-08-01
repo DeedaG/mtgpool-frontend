1c0a5d0d71443b7901f053f2405a426b53edd823
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 17:12:05 -0500

    created deletePool case and logic for delete

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -6,8 +6,10 @@ export default (state = [], action) => {
       return action.pools
     case "ADD_POOL":
       return state.concat(action.pool)
-      case "UPDATE_POOL":
-        return state
+    case "UPDATE_POOL":
+      return state.map(pool => pool.id === action.pool.id ? action.pool : pool)
+    case "DELETE_POOL":
+      return state.filter(pool => pool.id === action.poolId ? false : true)
     case "CLEAR_MY_POOLS":
       return initialState
 
