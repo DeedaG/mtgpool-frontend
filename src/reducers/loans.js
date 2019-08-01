3e3aa6704afada7c625ba70d8772ef57da0669fd
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 08:26:14 -0500

    removed add and removed cases

--- src/reducers/loans.js
+++ src/reducers/loans.js
@@ -1,8 +1,10 @@
 
 export default (state = [], action) => {
+  // const newLoan = action.loan;
   switch(action.type) {
     case "SET_MY_LOANS":
       return action.loans
+
     default:
         return state
     }
