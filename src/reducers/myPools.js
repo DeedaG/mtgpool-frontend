f2ddf886eefb621bacd479e92061b521b283b806
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 08:25:11 -0500

    included update pool case

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -6,6 +6,8 @@ export default (state = [], action) => {
       return action.pools
     case "ADD_POOL":
       return state.concat(action.pool)
+      case "UPDATE_POOL":
+        return state
     case "CLEAR_MY_POOLS":
       return initialState
 
