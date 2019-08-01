841aa1b0afc58cd6081fe047965ace09de2192eb
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 08:16:10 -0500

    referenced editpoolcontainer and newpoolcontainer instead of just newpoolform

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import NavBar from './components/NavBar.js';
-// import MainContainer from './components/MainContainer.js';
+import { setFormDataForEdit } from './actions/newPoolForm.js'
 import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser} from './actions/currentUser.js'
 import Login from './components/Login.js'
-// import Logout from './components/Logout.js'
+import NewPoolForm from './components/NewPoolForm.js'
 import MyPools from './components/MyPools.js'
 import MyLoans from './components/MyLoans.js'
 import PoolCard from './components/PoolCard.js'
 import Home from './components/Home.js'
-import NewPoolForm from './components/NewPoolForm.js'
+import EditPoolContainer from './containers/EditPoolContainer.js'
+import NewPoolContainer from './containers/NewPoolContainer.js'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
@@ -29,7 +30,7 @@ class App extends React.Component {
           <Route exact path='/login' component={Login}/>
           <Route exact path='/pools' component={MyPools}/>
           <Route exact path='/loans' component={MyLoans}/>
-          <Route exact path='/pools/new' component={NewPoolForm}/>
+          <Route exact path='/pools/new' component={NewPoolContainer}/>
           <Route exact path='/pools/:id' render={props => {
               const pool = pools.find(pool => pool.id === props.match.params.id)
                 return <PoolCard pool={pool} {...props}/>
@@ -37,24 +38,13 @@ class App extends React.Component {
               }/>
             <Route exact path='/pools/:id/edit' render={props => {
               const pool = pools.find(pool => pool.id ===
-              props.match.params.id)
-              return <NewPoolForm pool={pool} {...props}/>
+                props.match.params.id)
+              return <EditPoolContainer pool={pool} {...props}/>
               }
             }/>
        </Switch>
       </div>
-          // <Route exact path='/Pools/:id'
-            // <Route exact path='/pools/:id/edit' render={props => {
-            //     // I need to get ???
-            //     const pool = pools.find(pool => pool.id === props.match.params.id)
-            //     // dispatch updateForm -> trip
-            //     return <EditPoolFormWrapper pool={pool} {...props}/>
-            //   }
-            // }/>
 
-
-
-    // <Footer/>
     );
  }
 }
@@ -67,4 +57,4 @@ class App extends React.Component {
   }
 
 
-export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
+export default withRouter(connect(mapStateToProps, { getCurrentUser, setFormDataForEdit })(App));
