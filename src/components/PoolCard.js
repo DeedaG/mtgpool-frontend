8f60da2e75609d7ac5de42c1fff0d85e6afea8ab
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 08:23:58 -0500

    added list of loans in pool and investor id

--- src/components/PoolCard.js
+++ src/components/PoolCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const PoolCard = ({pool}) => {
+const PoolCard = ({pool, loan}) => {
   return (
     pool ?
     <div>
-      <h3>{pool.attributes.name}</h3>
-      <p>{pool.attributes.pool_amount}</p>
+      Pool Name:<h3>{pool.attributes.name}</h3>
+      Pool Amount: <p>{pool.attributes.pool_amount}</p>
+    Investor: <p>{pool.attributes.investor_id}</p>
+    Commited Loan Amounts:  <p>{pool.attributes.loans.map((loan) => (
+          <li>{loan.amount}</li> ))}</p>
+
       <Link to={`/pools/${pool.id}/edit`}>Edit this pool</Link><br></br>
-      <Link to={'/loans'}>Add Loans</Link>
+      <Link to={'/loans'}>Browse Loans</Link>
     </div> :
     null
   )
