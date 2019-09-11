921191d2ac78d14476c4d97803ac239dd524f490
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-09-10 22:07:04 -0500

    added sorting functions

--- src/components/MyPools.js
+++ src/components/MyPools.js
@@ -3,22 +3,107 @@ import { connect } from 'react-redux'
 // import PoolCard from './PoolCard.js'
 import { Link } from 'react-router-dom'
 
+class MyPools extends React.Component {
+  state = {
+    displaySorted: false,
+    inputNumber: 0,
+    compareNumber: 0
+   }
 
-const MyPools = props => {
-  const poolCards = props.pools.length > 0 ? props.pools.map(p =>
-   <><Link key={p.id} to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
+   handleChange=(event)=> {
+     this.setState({
+       inputNumber: event.target.value
+     })
+   }
+
+   handleSubmit =(event) => {
+     event.preventDefault()
+      this.setState({
+        compareNumber: this.state.inputNumber,
+        inputNumber: 0
+      })
+   }
+
+    handleClick = () => {
+       this.setState({
+         displaySorted: !this.state.displaySorted
+      })
+    }
+
+render() {
+
+      const allPoolCards = this.props.pools.length > 0 ? this.props.pools.map(p =>
+        <><Link to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
       <br></br><br></br></>) : null
-  return (
-    poolCards
-  )
-}
 
-const mapStateToProps = state => {
-  // debugger
-  return {
-    pools: state.myPools
-    // loans: state.loans
+      const greaterPools = this.props.pools.filter(p =>
+        p.attributes.pool_amount > this.state.compareNumber
+      ).map(p =>
+        <><Link to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
+      <br></br><br></br></>)
+
+      const abcPools = [...this.props.pools].sort(function(a, b) {
+        var nameA = a.attributes.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.attributes.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      }).map(p =>
+        <><Link to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
+    <br></br><br></br></>)
+
+      const abcGreaterPools = [...this.props.pools].filter(p =>
+        p.attributes.pool_amount > this.state.compareNumber
+      ).sort(function(a, b) {
+        var nameA = a.attributes.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.attributes.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      }).map(p =>
+        <><Link to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
+    <br></br><br></br></>)
+
+
+      return (
+          <div>
+            <h3><label>Current Pools</label></h3>
+            <p>{this.state.compareNumber > 0 && this.state.displaySorted ? abcGreaterPools
+                : (this.state.displaySorted ? abcPools
+                  : (this.state.compareNumber > 0 ? greaterPools : allPoolCards))}</p>
+                <h4><label>Sort Pools</label></h4>
+            <button onClick={this.handleClick}>ABC Sort</button>
+
+           <form onSubmit={this.handleSubmit}>
+
+             <input
+               placeholder="Search by Pool Amount"
+               onChange={this.handleChange}
+               value={this.state.number}>
+             </input>
+             <input type="submit" />
+           </form>
+
+          </div>
+        )
+      }
+    }
+
+
+  const mapStateToProps = state => {
+    return {
+      pools: state.myPools
+      // loans: state.loans
+    }
   }
-}
+
 
 export default connect(mapStateToProps)(MyPools)
