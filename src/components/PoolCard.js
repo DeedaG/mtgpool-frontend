a14e02961a9cb16530632af5da634286572539cc
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-09-11 19:04:51 -0500

    considering comment bar placement

--- src/components/PoolCard.js
+++ src/components/PoolCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import Comment from './Comment.js'
 // import { connect } from 'react-redux'
 
 const PoolCard = ({pool}) => {
@@ -12,9 +13,8 @@ const PoolCard = ({pool}) => {
     Investor: <p>{pool.attributes.investor_id}</p>
     Commited Loan Amounts:  <p>{pool.attributes.loans.map((loan) => (
           <li key={loan.id}>{loan.amount}</li> ))}</p>
-
       <Link to={`/pools/${pool.id}/edit`} >Edit this pool</Link><br></br>
-      <Link to={`/loans`}>Browse loans</Link><br></br>
+      <Link to={`/loans`}>Browse loans</Link><br></br><br></br>
     </div> :
     null
   )
