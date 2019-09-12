172884d807808aca5ebba8a14f15580f3e2b9eb4
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-09-11 19:03:42 -0500

    considering where to put comment bar

--- src/components/MyPools.js
+++ src/components/MyPools.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Comment from './Comment.js'
 import { Link } from 'react-router-dom'
 
 class MyPools extends React.Component {
@@ -34,7 +33,6 @@ render() {
 
       const allPoolCards = this.props.pools.length > 0 ? this.props.pools.map(p =>
         <><Link to ={`/pools/${p.id}`} >{p.attributes.name}</Link>
-        <Comment />
       <br></br><br></br></>) : null
 
       const greaterPools = this.props.pools.filter(p =>
