2f19cd4e1e650c1bdd8327d35a62cc4ddc02eac1
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-09-13 12:02:40 -0500

    did not save changes here

--- src/components/LoanCard.js
+++ src/components/LoanCard.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { connect } from 'react-redux'
 
 
 const LoanCard = ({loan}) => {
@@ -8,21 +7,12 @@ const LoanCard = ({loan}) => {
     <div>
       Borrower:<h3>{loan.attributes.borrower}</h3>
       Loan Amount: <p>{loan.attributes.amount}</p>
-      Pool Id: <p>{loan.attributes.pool_id}</p>
+    Pool Id: <p>{loan.attributes.pool_id}</p>
 
       </div> : null
 
     )
-
-
-// const mapStateToProps = state => {
-//   const poolId = state.loans.pool_id
-  // debugger
-  // return {
-  //   // pools: state.myPools,
-  //   LoanCard
-  //
-  // }
 }
+
 
 export default LoanCard;
