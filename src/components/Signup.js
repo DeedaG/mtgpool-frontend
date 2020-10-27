ff7f66f72e3200eee1c7ee31afbd69a8591860b2
Christia <ChristiaTidwell@gmail.com>
2020-10-26 20:47:22 -0500

    signup code

---
+++ src/components/Signup.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateSignupForm } from "../actions/signupForm.js"
+import { signup } from "../actions/currentUser.js"
+
+
+const Signup = ({ signupFormData, updateSignupForm, signup, history }) => {
+
+  const handleUser = event => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    signup(signupFormData, history)
+  }
+
+  const mystyle = {
+        color: "black",
+        backgroundColor: "silver",
+        padding: "8px",
+        fontFamily: "Arial"
+      };
+    // debugger
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input style={mystyle} placeholder="username" value={signupFormData.username} name="username" type="text" onChange={handleUser} />
+      <input style={mystyle} placeholder="password" value={signupFormData.password} name="password" type="text" onChange={handleUser} />
+    <br/>
+      <input style={mystyle}  type="submit" value="Sign Up"/>
+    </form>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    signupFormData: state.signupForm
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm, signup } )(Signup)
