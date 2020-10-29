25c5944d6acc97f684345645b62dcdecb9064143
Christia <ChristiaTidwell@gmail.com>
2020-10-29 11:01:26 -0500

    refining investor actions

--- src/actions/investors.js
+++ src/actions/investors.js
@@ -1,8 +1,38 @@
+import { resetNewInvestorForm } from './newInvestorForm'
+
 export const setMyInvestors = investors => {
-  debbuggers
   return {
     type: "SET_MY_INVESTORS",
     investors
+  }
+}
+
+export const clearMyInvestors = () => {
+  return {
+    type: "CLEAR_MY_INVESTORS"
+  }
+}
+
+export const addInvestor = investor => {
+  return {
+    type: 'ADD_INVESTOR',
+    investor
+  }
+}
+
+
+export const deleteInvestorSuccess = investorId => {
+  return {
+    type: 'DELETE_INVESTOR',
+    investorId
+  }
+}
+
+
+export const updateInvestorSuccess = investor => {
+  return {
+    type: 'UPDATE_INVESTOR',
+    investor
   }
 }
 
@@ -27,5 +57,83 @@ export const getMyInvestors = () => {
       })
       .catch(console.log)
   }
+}
 
+export const createInvestor = ( investorData, history ) => {
+  return dispatch => {
+    console.log(investorData)
+    const sendableInvestorData = {
+        name: investorData.name,
+        fee: investorData.fee
+    }
+    return fetch('http://localhost:3000/api/v1/investors', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableInvestorData)
+    })
+    .then(r => r.json())
+    .then(resp => {
+      console.log("resp", resp)
+      if (resp.error) {
+        alert(resp.error)
+      }else {
+      dispatch(addInvestor(resp.data))
+      dispatch(resetNewInvestorForm())
+      history.push(`/investors/${resp.data.id}`)
+     }
+    })
+  }
+}
+
+export const updateInvestor = ( investorData, history ) => {
+  return dispatch => {
+    console.log(investorData)
+    const sendableInvestorData = {
+        id: investorData.id,
+        name: investorData.name,
+        fee: investorData.fee
+    }
+    return fetch(`http://localhost:3000/api/v1/pools/${investorData.investorId}`, {
+      credentials: 'include',
+      method: 'PATCH',
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableInvestorData)
+    })
+    .then(r => r.json())
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      }else {
+      dispatch(updateInvestorSuccess(resp.data))
+      history.push(`/investors/${resp.data.id}`)
+     }
+    })
+  }
+}
+
+export const deleteInvestor = (investorId, history) => {
+  return dispatch => {
+    return fetch(`http://localhost:3000/api/v1/investors/${investorId}`, {
+      credentials: "include",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(r => r.json())
+      .then(resp => {
+        if (resp.error) {
+          alert(resp.error)
+        } else {
+          dispatch(deleteInvestorSuccess(investorId))
+          history.push(`/investors`)
+        }
+      })
+      .catch(console.log)
+  }
 }
