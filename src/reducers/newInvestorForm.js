0995e70d9d8a27dcd2e83965cd9643e9c3d24c80
Christia <ChristiaTidwell@gmail.com>
2020-10-29 11:10:01 -0500

    included matching redure for investorForm

---
+++ src/reducers/newInvestorForm.js
@@ -0,0 +1,21 @@
+const initialState = {
+    name: "",
+    fee: ""
+}
+
+export default (state=initialState, action) => {
+
+  switch (action.type) {
+    case 'UPDATE_NEW_INVESTOR_FORM':
+      return {
+        ...state,
+        [action.formData.name]: action.formData.value }
+    case 'RESET_NEW_INVESTOR_FORM':
+      return initialState
+    case "SET_FORM_DATA_FOR_EDIT":
+        return action.investorFormData
+
+    default:
+      return state
+  }
+}
