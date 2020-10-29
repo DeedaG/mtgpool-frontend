a8d12f94ffc678be4f448eb984b6d64dc63db388
Christia <ChristiaTidwell@gmail.com>
2020-10-29 10:59:30 -0500

    mixed up commits, changed button and investor list css to App.cs

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ import MyInvestors from './components/MyInvestors.js'
 import LoanCard from './components/LoanCard.js'
 import PoolCard from './components/PoolCard.js'
 import Home from './components/Home.js'
+import NewInvestorContainer from './containers/NewInvestorContainer.js'
 import EditPoolContainer from './containers/EditPoolContainer.js'
-import NewPoolContainer from './containers/NewPoolContainer.js'
+import EditInvestorContainer from './containers/EditPoolContainer.js'
+import NewPoolContainer from './containers/NewInvestorContainer.js'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
@@ -24,7 +26,6 @@ class App extends React.Component {
 
   render() {
     const { loggedIn, pools, loans, investors } = this.props
-    debugger
     return (
       <div className="App">
         { loggedIn ? <NavBar/> : <Home/> }
@@ -35,7 +36,8 @@ class App extends React.Component {
           <Route exact path='/pools/new' component={NewPoolContainer}/>
           <Route exact path='/pools/:id' render={props => {
               const pool = pools.find(pool => pool.id === props.match.params.id)
-                return <PoolCard pool={pool} investors={investors} {...props}/>
+              const investor = pool ? investors.find(inv => inv.id === pool.attributes.investor_id.toString()) : null
+                return <PoolCard pool={pool} investor={investor} {...props}/>
                 }
               }></Route>
           <Route exact path='/pools/:id/edit' render={props => {
@@ -44,21 +46,27 @@ class App extends React.Component {
             return <EditPoolContainer pool={pool} {...props}/>
             }
           }/>
-
           <Route exact path='/loans' component={MyLoans}/>
-          <Route exact path='/investors' component={MyInvestors}/>
           <Route exact path='/loans/:id/edit' render={props => {
               const loan = loans.find(loan => loan.id === props.match.params.id)
             return <LoanCard loan={loan} {...props}/>
               }
             }></Route>
-
-
-
-            />
+          <Route exact path='/investors' component={MyInvestors}/>
+            <Route exact path='/investors/new' component={NewInvestorContainer}/>
+            <Route exact path='/investors/:id' render={props => {
+              const investor = investors.find(investor => investor.id === props.match.params.id)
+                return <MyInvestors investor={investor} investors={investors} {...props}/>
+              }}>
+            </Route>
+            <Route exact path='/investors/:id/edit' render={props => {
+              const investor = investors.find(investor => investor.id ===
+                props.match.params.id)
+              return <EditInvestorContainer investor={investor} {...props}/>
+              }
+            }/>
        </Switch>
       </div>
-
     );
  }
 }
@@ -67,7 +75,8 @@ class App extends React.Component {
   return ({
     loggedIn: !!state.currentUser,
     pools: state.myPools,
-    loans: state.loans
+    loans: state.loans,
+    investors: state.investors
     })
   }
 
