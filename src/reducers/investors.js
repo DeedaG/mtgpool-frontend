affacc0926b0924edc5b85c5a49d65c2c6b1e6d9
Christia <ChristiaTidwell@gmail.com>
2020-10-29 11:04:59 -0500

    included matching reducers for actions

--- src/reducers/investors.js
+++ src/reducers/investors.js
@@ -1,10 +1,19 @@
-export default (state = [], action) => {
+   const initialState = []
 
-  switch(action.type) {
-    case "SET_MY_INVESTORS":
-      return action.investors
+   export default (state = [], action) => {
+     switch(action.type) {
+       case "SET_MY_INVESTORS":
+         return action.investors
+       case "ADD_INVESTOR":
+         return state.concat(action.investor)
+       case "UPDATE_INVESTOR":
+         return state.map(investor => investor.id === action.investor.id ? action.investor : investor)
+       case "DELETE_INVESTOR":
+         return state.filter(investor => investor.id === action.investorId ? false : true)
+       case "CLEAR_MY_INVESTORS":
+         return initialState
 
-    default:
-        return state
-    }
-   }
+       default:
+           return state
+       }
+     }
