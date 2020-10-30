8ed0c9b00b373a4708fa442bb4302e60570ab686
Christia <ChristiaTidwell@gmail.com>
2020-10-29 21:56:26 -0500

    changed button colors

--- src/components/NewPoolForm.js
+++ src/components/NewPoolForm.js
@@ -44,7 +44,7 @@ const NewPoolForm = ({ formData, updateNewPoolForm, userId, handleSubmit, editMo
         <br></br>
         <LoanCheckbox />
           <input
-            className="button2"
+            className={editMode ? "button3" : "button2"}
             type="submit"
             value={editMode ? "Update Pool" : "Create Pool"}
           />
