6f716599f117f2e8684fd0da6befd952792b82a3
Christia <ChristiaTidwell@gmail.com>
2020-10-29 21:58:49 -0500

    wrapped alert provider around app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
@@ -8,9 +10,28 @@ import store from './store.js'
 import { BrowserRouter } from 'react-router-dom'
 
 
+const options = {
+  position: 'top center',
+  // timeout: 5000,
+  offset: '30px',
+  transition: 'scale'
+}
+
+// const Root = () => (
+//   <AlertProvider template={AlertTemplate} {...options}>
+//     <App />
+//   </AlertProvider>
+// )
+
+// render(<Root />, document.getElementById('root'))
+
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
 
