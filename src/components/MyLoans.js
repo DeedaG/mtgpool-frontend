457bacabe09c0d28d7c6c5c945bc9b8adfb0022a
Christia <ChristiaTidwell@gmail.com>
2020-10-29 23:15:41 -0500

    set up table and added commas to amount values

--- src/components/MyLoans.js
+++ src/components/MyLoans.js
@@ -6,13 +6,36 @@ import { Link } from 'react-router-dom'
 
 const MyLoans = ({loans, pools}) => {
 
-  const loanCards =  loans.length > 0 ? loans.map(loan =>
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  }
 
-    	<li key = {loan.id}><><Link to ={`/loans/${loan.id}/edit`} key={loan.attributes.id}>
-          {loan.attributes.borrower} - ${loan.attributes.amount}
-          - {loan.attributes.rate}% - {loan.attributes.term}yr -
-          Pool: {pools.filter(pool => pool.attributes.id === loan.attributes.pool_id).map(p => p.attributes.name)}
-        </Link><br></br><br></br></></li>) : null
+  const loanCards =  loans.length > 0 ? loans.map(loan =>
+    <table className="pools" key = {loan.id}>
+      <thead>
+        <tr>
+          <th>Borrower Name</th>
+          <th>Loan Amount</th>
+          <th>Interest Rate</th>
+          <th>Term</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <Link to ={`/loans/${loan.id}/edit`} key={loan.attributes.id} style={{color: "green"}}>
+              {loan.attributes.borrower}
+            </Link></td>
+          <td>${numberWithCommas(loan.attributes.amount.toFixed(2))}</td>
+          <td>{loan.attributes.rate}%</td>
+          <td>{loan.attributes.term}yr</td>
+        </tr>
+      </tbody>
+    </table>
+        //   Pool: {pools.filter(pool => pool.attributes.id === loan.attributes.pool_id).map(p => p.attributes.name)}
+        // </Link><br></br><br></br></></li>
+      )
+        : null
 
   return (
     loanCards
@@ -20,7 +43,6 @@ const MyLoans = ({loans, pools}) => {
 }
 
   const mapStateToProps = state => {
-
     return {
       loans: state.loans,
       pools: state.myPools
