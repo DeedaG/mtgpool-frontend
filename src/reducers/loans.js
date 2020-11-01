f6188c765b370b670cac52de35c80eb83af39049
Christia <ChristiaTidwell@gmail.com>
2020-10-31 23:06:20 -0500

    add matching reducers for adding loans

--- src/reducers/loans.js
+++ src/reducers/loans.js
@@ -1,10 +1,18 @@
+const initialState = []
 
 export default (state = [], action) => {
 
   switch(action.type) {
     case "SET_MY_LOANS":
       return action.loans
-
+      case "ADD_LOAN":
+        return state.concat(action.loan)
+      case "UPDATE_LOAN":
+        return state.map(loan => loan.id === action.loan.id ? action.loan : loan)
+      case "DELETE_LOAN":
+        return state.filter(loan => loan.id === action.loanId ? false : true)
+      case "CLEAR_MY_LOANS":
+        return initialState
     default:
         return state
     }
