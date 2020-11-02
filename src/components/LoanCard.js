f0472af9f46a1bfc57695480a5fa43832e9e7665
Christia <ChristiaTidwell@gmail.com>
2020-11-01 20:20:23 -0600

    add edit link

--- src/components/LoanCard.js
+++ src/components/LoanCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const LoanCard = ({loan}) => {
 
@@ -13,15 +13,19 @@ const LoanCard = ({loan}) => {
 
           <h3>Borrower: {loan.attributes.borrower}</h3>
           <h3>Loan Number: {loan.id}</h3>
-            <p>Pool Number: {loan.attributes.pool_id > 0 ? loan.attributes.pool_id :
+          <p>Pool Number: {loan.attributes.pool_id > 0 ? loan.attributes.pool_id :
                 <span style={{color: "red"}}>Not Committed</span>}
-            </p>  
+            </p>
           <p>Loan Amount: ${numberWithCommas(loan.attributes.amount.toFixed(2))}</p>
           <p>Interest Rate: {loan.attributes.rate}%</p>
           <p>Term: {loan.attributes.term} yr</p>
           <p>Closing Date: {loan.attributes.close_date}</p>
 
-        </div>
+        <Link to ={`/loans/${loan.id}/edit`}>
+          <button className="button3">Edit this Loan</button>
+          <br></br><br></br>
+        </Link>
+    </div>
      : null
     )
 }
