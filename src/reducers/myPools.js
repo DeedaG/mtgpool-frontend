d54b3f966fcb44d0903a22c9a57dfa6f8e92094e
Christia <ChristiaTidwell@gmail.com>
2020-11-11 10:06:58 -0600

    removed more console.logs

--- src/reducers/myPools.js
+++ src/reducers/myPools.js
@@ -7,8 +7,6 @@ export default (state = [], action) => {
     case "ADD_POOL":
       return state.concat(action.pool)
     case "UPDATE_POOL":
-debugger
-console.log("state", state)
       return state.map(pool => pool.id === action.pool.id ? action.pool : pool)
     case "DELETE_POOL":
       return state.filter(pool => pool.id === action.poolId ? false : true)
